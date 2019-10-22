f258a05d5c5bec25be6ff6e66621757cfe5f0413
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-10-22 08:58:49 -0500

    MVN: Maven Structure initiated

---
+++ myday/src/main/java/edu/baylor/ecs/sw1/myday/App.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.sw1.myday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
