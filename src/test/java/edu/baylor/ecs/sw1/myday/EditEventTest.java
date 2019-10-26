383b2b93b181ed9dc9f6f19335a787de7e81afc7
Par Wilkinson <ParWilkinson@gmail.com>
2019-10-26 09:58:23 -0700

    Created and completed EventEditor dialog

---
+++ src/test/java/edu/baylor/ecs/sw1/myday/EditEventTest.java
@@ -0,0 +1,43 @@
+package edu.baylor.ecs.sw1.myday;
+
+import javax.swing.JFrame;
+
+import edu.baylor.ecs.sw1.event.*;
+
+public class EditEventTest {
+	public static void printEvent(Event event) {
+		System.out.println("Event Name: " + event.getEventName());
+		System.out.println("Event Priority: " + event.getEventPriority());
+		System.out.println("Event Start Date: " + event.getStartDate().toString());
+		System.out.println("Event End Date: " + event.getEndDate().toString());
+		System.out.println("Event Description: " + event.getEventDescription());
+	}
+	
+	public static void main(String args[]) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame testFrame = new JFrame();
+				testFrame.setVisible(true);
+				testFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				testFrame.pack();
+				
+				
+				System.out.println("Creating event...");
+				EventCreator creator = new EventCreator(testFrame);
+				
+				Event event = creator.getEvent();
+				if(event != null) {
+					printEvent(event);
+					
+					System.out.println("\nEditing event...");
+					EventEditor editor = new EventEditor();
+					editor.edit(testFrame, event);
+					
+					printEvent(event);
+				} else {
+					System.out.println("User cancelled event creation.");
+				}
+			}
+		});
+	}
+}
