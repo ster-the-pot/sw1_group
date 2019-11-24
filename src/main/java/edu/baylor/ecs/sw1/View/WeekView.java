6f3b60f16e541854cad72cf3341b3e86f9ad227d
Elizabeth Brighton <egbrighton@gmail.com>
2019-11-24 13:44:02 -0600

    SWING: changed way View interacts with list of events

--- src/main/java/edu/baylor/ecs/sw1/View/WeekView.java
+++ src/main/java/edu/baylor/ecs/sw1/View/WeekView.java
@@ -1,7 +1,5 @@
 package edu.baylor.ecs.sw1.View;
 
-
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -9,6 +7,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.swing.BorderFactory;
@@ -20,12 +19,11 @@ import javax.swing.SwingConstants;
 
 import edu.baylor.ecs.sw1.scheduleRender.ShowDay;
 
-
 /**
- * WeekView class creates the week view for the calendar. 
- * It uses a JPanel as the calendar base. The main differences
- * between this and the MonthView class is how the tables
- * are made and updated, as well as the Action performed Override.
+ * WeekView class creates the week view for the calendar. It uses a JPanel as
+ * the calendar base. The main differences between this and the MonthView class
+ * is how the tables are made and updated, as well as the Action performed
+ * Override.
  * 
  * @author Elizabeth Brighton
  *
@@ -34,26 +32,21 @@ public class WeekView extends View {
 
 	static int realYear, realMonth, realDay, currentDay, lastYear, lastMonth, lastMaxDay, dayOfWeek;
 	static JPanel panel;
-	
-	
 
 	protected void initCalendar() {
 		panel = new JPanel();
-		
+
 		panel.setBackground(Color.darkGray);
-		//panel.setSize(500, 700);
+		// panel.setSize(500, 700);
 		panel.setPreferredSize(new Dimension(1100, 600));
-		panel.setMaximumSize( panel.getPreferredSize() );
+		panel.setMaximumSize(panel.getPreferredSize());
 		GridLayout layout = new GridLayout(0, 7);
 		layout.setHgap(1);
 		layout.setVgap(1);
-		
+
 		panel.setLayout(layout);
 		panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		
-		
 
-		
 		// Get current day
 		cal = new GregorianCalendar(); // Create calendar
 		realDay = cal.get(GregorianCalendar.DAY_OF_MONTH); // Get day
@@ -62,24 +55,19 @@ public class WeekView extends View {
 		currentMonth = realMonth; // Match month and year
 		currentYear = realYear;
 		currentDay = realDay;
-		
 
 		mMonth.setText(months[currentMonth] + " " + currentYear);
 
-
 		int numDays;
 		numDays = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-		if(currentDay - 7 < 0) {
+		if (currentDay - 7 < 0) {
 			dayOfWeek = cal.get(GregorianCalendar.DAY_OF_WEEK_IN_MONTH) - cal.get(GregorianCalendar.DAY_OF_WEEK);
 		} else {
 			dayOfWeek = cal.get(GregorianCalendar.DAY_OF_WEEK);
 		}
-		
-		
-		
-		//Function that adds all the Panels
+
+		// Function that adds all the Panels
 		addPanels();
-		
 
 	}
 
@@ -88,50 +76,51 @@ public class WeekView extends View {
 		mMonth.setText(months[currentMonth] + " " + currentYear);
 
 		panel.removeAll();
-	
-		
-		
-		//Function that adds all the Panels
+
+		// Function that adds all the Panels
 		addPanels();
-		//initCalendar();
-		
+		// initCalendar();
+
 		panel.revalidate();
 		panel.repaint();
 	}
 
 	protected void addPanels() {
-		
+
 		int numDays;
 		numDays = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
 
 		int i = 1;
 		for (i = (currentDay - dayOfWeek + 1); i <= (currentDay + 7 - dayOfWeek); i++) {
-			if(i > numDays) {	
-				if(currentMonth != 11)
-					panel.add(new ShowDay((i  - numDays), currentMonth+1, currentYear, true));
-				else
-					panel.add(new ShowDay((i - numDays), 0, currentYear+1, true));
-				
-			} else if (i <= 0){
-				if(currentMonth != 0)
-					panel.add(new ShowDay((lastMaxDay + i), currentMonth-1, currentYear, true));	
-				else 
-					panel.add(new ShowDay((lastMaxDay + i), 11, currentYear-1, true));
 
-			}else {
-				panel.add(new ShowDay(i, currentMonth, currentYear, false));
+			if (i > numDays) {
+				if (currentMonth != 11) {
+					dayDate = new Date(currentYear, currentMonth + 1, (i - numDays));
+					panel.add(new ShowDay((i - numDays), currentMonth + 1, dayDate, getDayEvents(dayDate), true));
+				} else {
+					dayDate = new Date(currentYear + 1, 0, (i - numDays));
+					panel.add(new ShowDay((i - numDays), 0, dayDate, getDayEvents(dayDate), true));
+				}
+			} else if (i <= 0) {
+				if (currentMonth != 0) {
+					dayDate = new Date(currentYear, currentMonth - 1, (i + lastMaxDay));
+					panel.add(new ShowDay((lastMaxDay + i), currentMonth - 1, dayDate, getDayEvents(dayDate), true));
+				} else {
+					dayDate = new Date(currentYear-1, 11, (i + lastMaxDay));
+					panel.add(new ShowDay((lastMaxDay + i), 11, dayDate, getDayEvents(dayDate), true));
+				}
+			} else {
+				dayDate = new Date(currentYear, currentMonth, (i));
+				panel.add(new ShowDay(i, currentMonth, dayDate, getDayEvents(dayDate), false));
 			}
 		}
-		
+
 		mPanel.add(panel);
 		this.add(mPanel);
 		this.setBackground(Color.WHITE);
 		mPanel.setVisible(true);
 	}
-	
-	
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if ("Last".equals(e.getActionCommand())) {
@@ -140,7 +129,7 @@ public class WeekView extends View {
 					currentMonth--;
 					lastMonth--;
 					lastMaxDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-					
+
 					cal = new GregorianCalendar(currentYear, currentMonth, 1);
 					currentDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) - 7 + currentDay;
 
@@ -150,7 +139,7 @@ public class WeekView extends View {
 					currentMonth = 11;
 					currentYear--;
 					lastMaxDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-					
+
 					cal = new GregorianCalendar(currentYear, currentMonth, 1);
 					currentDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) - 7 + currentDay;
 
@@ -158,38 +147,33 @@ public class WeekView extends View {
 			} else {
 				currentDay = currentDay - 7;
 			}
-		//If the calendar is moving forward we don't have to worry about last month
+			// If the calendar is moving forward we don't have to worry about last month
 		} else if ("Next".equals(e.getActionCommand())) {
 			if (currentDay + 7 > cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH)) {
 				if (currentMonth != 11) {
 					lastMonth = currentMonth;
 					currentMonth++;
 					lastMaxDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-					
-					
-					int currentDisp = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) 
-							- currentDay;
-					
+
+					int currentDisp = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) - currentDay;
+
 					cal = new GregorianCalendar(currentYear, currentMonth, 1);
 
-					currentDay = 7- currentDisp;	
-					
-					
+					currentDay = 7 - currentDisp;
+
 				} else {
 					lastMonth = currentMonth;
 					lastYear = currentYear;
 					lastMaxDay = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-					
+
 					currentMonth = 0;
 					currentYear++;
-					
-					int currentDisp = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) 
-							- currentDay;
+
+					int currentDisp = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH) - currentDay;
 					cal = new GregorianCalendar(currentYear, currentMonth, 1);
 
-					
-					currentDay = 7 - currentDisp;	
-					
+					currentDay = 7 - currentDisp;
+
 				}
 			} else {
 				currentDay = currentDay + 7;
@@ -197,6 +181,5 @@ public class WeekView extends View {
 		}
 		updateCalendar();
 	}
-	
 
 }
