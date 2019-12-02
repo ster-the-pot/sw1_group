7edf61bf4f02f47ea1d8803bc2a57f05f30f366b
Sterling Trafford <Sterling_Trafford1@baylor.edu>
2019-12-02 13:33:56 -0600

    DB: external database setup

--- src/main/java/edu/baylor/ecs/sw1/auth/AuthService.java
+++ src/main/java/edu/baylor/ecs/sw1/auth/AuthService.java
@@ -32,7 +32,7 @@ public class AuthService {
 	private AuthService(String user, String db, char[] password) {
 
 		MongoCredential credential = MongoCredential.createCredential(user, db, password);
-		client = new MongoClient(new ServerAddress("localhost", 27017), Arrays.asList(credential));
+		client = new MongoClient(new ServerAddress("18.224.202.17", 27017), Arrays.asList(credential));
 		this.db = client.getDatabase("userdata");
 		this.userdata = this.db.getCollection("userdata");
 	}
